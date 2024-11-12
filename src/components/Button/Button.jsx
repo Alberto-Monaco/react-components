a97c85ff348b8aa046bc89756cae812047e41f81
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-12 15:47:19 +0100

    fix button class

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import style from '../Button/Button.module.css'
+
 export default function Button() {
-	return <button className={style.button}>LEGGI DI PIÙ</button>
+	return <button className={`${style.button} rounded-0 text-uppercase text-white px-4 btn `}>Leggi di più</button>
 }
