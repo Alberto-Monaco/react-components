0350d7c051ba56bfa8c1b06e1c825f8bae8c545f
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-12 14:32:38 +0100

    fix main and add footer

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,6 @@
 export default function AppHeader() {
 	return (
-		<header className='header text-center py-2 bg-light'>
+		<header className='header text-center py-3 bg-light'>
 			<h1>Il mio blog</h1>
 		</header>
 	)
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,19 +2,19 @@ import img from '../assets/img/600x400.png'
 
 export default function AppMain() {
 	return (
-		<main className='main bg-light vh-100'>
-			<div className='container'>
+		<main className='main bg-light py-3 '>
+			<div className='container py-5'>
 				<div className='row justify-content-center'>
 					<div className='col-6'>
-						<div className='card border-1 border-light-subtle rounded'>
+						<div className='card border-1 border-light-subtle shadow'>
 							<img src={img} alt='card-img' className='card-img rounded-top rounded-0' />
 							<div className='card-body'>
 								<h5 className='card-title'>Titolo del post</h5>
 								<p className='card-text'>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat asperiores nam unde hic, consectetur
-									nemo.
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptates possimus quas esse ab
+									adipisci nostrum similique commodi pariatur nihil, ad id accusamus, omnis dignissimos!
 								</p>
-								<button className='btn btn-warning text-uppercase px-4 text-white'>LEGGI DI PIù</button>
+								<button className='btn btn-warning text-uppercase px-4 text-white rounded-0'>LEGGI DI PIù</button>
 							</div>
 						</div>
 					</div>
