476280825fdc14d8c1548489ba709fbf069d9f95
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-12 14:53:21 +0100

    add card components

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,4 +1,4 @@
-import img from '../assets/img/600x400.png'
+import Card from './Card'
 
 export default function AppMain() {
 	return (
@@ -6,17 +6,7 @@ export default function AppMain() {
 			<div className='container'>
 				<div className='row justify-content-center'>
 					<div className='col-6'>
-						<div className='card border-1 border-light-subtle shadow'>
-							<img src={img} alt='card-img' className='card-img rounded-top rounded-0' />
-							<div className='card-body'>
-								<h5 className='card-title'>Titolo del post</h5>
-								<p className='card-text'>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptates possimus quas esse ab
-									adipisci nostrum similique commodi pariatur nihil, ad id accusamus, omnis dignissimos!
-								</p>
-								<button className='btn btn-warning text-uppercase px-4 text-white rounded-0'>LEGGI DI PIù</button>
-							</div>
-						</div>
+						<Card />
 					</div>
 				</div>
 			</div>
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import img from '../assets/img/600x400.png'
+
+export default function Card() {
+	return (
+		<div className='card border-1 border-light-subtle shadow'>
+			<img src={img} alt='card-img' className='card-img rounded-top rounded-0' />
+			<div className='card-body'>
+				<h5 className='card-title'>Titolo del post</h5>
+				<p className='card-text'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptates possimus quas esse ab adipisci
+					nostrum similique commodi pariatur nihil, ad id accusamus, omnis dignissimos!
+				</p>
+				<button className='btn btn-warning text-uppercase px-4 text-white rounded-0'>LEGGI DI PIù</button>
+			</div>
+		</div>
+	)
+}
